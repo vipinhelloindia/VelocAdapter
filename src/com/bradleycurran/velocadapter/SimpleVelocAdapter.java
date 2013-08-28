6c47a8a891bb85508b82c46cf4e3e5b44140972e
Brad <brad@bradleycurran.com>
2013-08-28 18:46:59 +1000

    Added SimpleVelocAdapter for simple implementations.

---
+++ src/com/bradleycurran/velocadapter/SimpleVelocAdapter.java
@@ -0,0 +1,28 @@
+package com.bradleycurran.velocadapter;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public abstract class SimpleVelocAdapter<T> extends ArrayVelocAdapter<T> {
+
+    private int mLayoutId;
+
+    public SimpleVelocAdapter(Context context, int layoutId) {
+        super(context);
+        mLayoutId = layoutId;
+    }
+
+    public SimpleVelocAdapter(Context context, int layoutId, List<T> list) {
+        super(context, list);
+        mLayoutId = layoutId;
+    }
+
+    @Override
+    protected View newView(LayoutInflater inflator, int position, ViewGroup parent) {
+        return inflator.inflate(mLayoutId, parent, false);
+    }
+}
